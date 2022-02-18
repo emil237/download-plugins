19fdcf1913ed22dca6f5de417573f8c138d34f11
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-18 11:43:26 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -37,17 +37,34 @@ echo " "*********************************************************"
 echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
 sleep 4;
 	echo '========================================================================================================================='
+###########################################                                                                         #!/bin/sh
+#DESCRIPTION=This script will download plugins to hdd
+####################################################################################
+ # # Command: wget https://raw.githubusercontent.com/emil237/download-plugins/main/installer.sh -qO - | /bin/sh # #
+###################################################################################
+wget https://www.dropbox.com/s/fzxuq5yl5m3ttee/download-plugins.tar.bz2?dl=0 -O /tmp/download-plugins.tar.bz2
+sleep 1
+echo "Installing Plugin..."
+tar -jxvf /tmp/download-plugins.tar.bz2 -C /
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1
+rm /tmp/download-plugins.tar.bz2
+echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
+sleep 4;
+	echo '========================================================================================================================='
 ###########################################                                                                                                                  
 echo ". >>>>         RESTARING     <<<<"
 echo "**********************************************************************************"
 wait
 killall -9 enigma2
 exit 0
-
-
-
-
-
 
 
 
