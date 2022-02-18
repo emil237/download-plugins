724d893df347e859ae9e52dc42223b1ca1194012
emil237 <95539537+emil237@users.noreply.github.com>
2022-02-18 11:47:42 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,43 +1,5 @@
-#!/bin/sh # 
- # # Command: wget https://raw.githubusercontent.com/emil237/download-plugins/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL="https://raw.githubusercontent.com/emil237/download-plugins/main"  
-echo "******************************************************************************************************************"
-echo "    download plugins to hdd   "
-echo "============================================================================================================================="
-#####################################################################################
-echo "      download plugins to hdd    "
-cd /tmp
-set -e 
-wget -q "--no-check-certificate" "https://drive.google.com/uc?id=1RTeS5tq8S7yLvgZdHKCQbVrWjZJEWdkz&export=download"
-wait
-tar xzvpf download-plugins.tar.gz  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/download-plugins.tar.gz
-echo "==========================================================================================================================="
-echo "      download plugins to hdd    "
-cd /tmp
-set -e 
-wget -q "--no-check-certificate"  "https://drive.google.com/uc?id=1ReNewIEl1jNYIIS5aVpRyZAa4jgxolzg&export=download"
-wait
-tar xzvpf download-plugins2.tar.gz  -C /
-wait
-cd ..
-set +e
-rm -f /tmp/download-plugins2.tar.gz
-sleep 2;
-echo "" 
-echo "" 
-echo "****************************************************************************************************************************"
-echo "# DOWNLOADS  INSTALLED SUCCESSFULLY #"
-echo "
-echo " "*********************************************************" 
-	echo "********************************************************************************"
-echo "   UPLOADED BY  >>>>   EMIL_NABIL "   
-sleep 4;
-	echo '========================================================================================================================='
-###########################################                                                                         #!/bin/sh
+#!/bin/sh
+####
 #DESCRIPTION=This script will download plugins to hdd
 ####################################################################################
  # # Command: wget https://raw.githubusercontent.com/emil237/download-plugins/main/installer.sh -qO - | /bin/sh # #
@@ -65,6 +27,7 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
 
 
 
