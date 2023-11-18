757a713542d99951915d2d19dd7e56b0ab3c08b3
emil237 <marianatalaat1@gmail.com>
2023-11-18 20:40:22 +0200

    Add files via upload

--- multi-plugins.sh
+++ multi-plugins.sh
@@ -16,55 +16,55 @@ echo "  1 - Cccam"
     IFS='-'
     read -r choice
     
-if [ $choice -eq 1 ]
+if $choice -eq 1
 then
 sleep 1
 echo " download emu cccam "
 wget https://dreambox4u.com/emilnabil237/emu/installer-cccam.sh -O - | /bin/sh
 
-elif [ $choice -eq 2 ]
+elif $choice -eq 2
 then
 sleep 1
 echo " download emu ncam "
 wget https://dreambox4u.com/emilnabil237/emu/installer-ncam.sh -O - | /bin/sh
 
-elif [ $choice -eq 3 ]
+elif $choice -eq 3
 then
 sleep 1
 echo " download emu oscam "
 wget https://dreambox4u.com/emilnabil237/emu/installer-oscam.sh -O - | /bin/sh
 
-elif [ $choice -eq 4 ]
+elif $choice -eq 4
 then
 sleep 1
 echo " download plugin Raedquicksignal "
 wget https://dreambox4u.com/emilnabil237/plugins/RaedQuickSignal/installer.sh -O - | /bin/sh
 
-elif [ $choice -eq 5 ]
+elif $choice -eq 5
 then
 sleep 1
 echo " download plugin Arabicsavior "
 wget http://dreambox4u.com/emilnabil237/plugins/ArabicSavior/installer.sh -O - | /bin/sh
 
-elif [ $choice -eq 6 ]
+elif $choice -eq 6
 then
 sleep 1
 echo " download plugin Ajpanel "
 wget -q "--no-check-certificate" http://dreambox4u.com/emilnabil237/plugins/ajpanel/installer.sh -O - | /bin/sh
 
-elif [ $choice -eq 7 ]
+elif $choice -eq 7
 then
 sleep 1
 echo " download plugin Dreamsat Panel "
 wget -q "--no-check-certificate" http://dreambox4u.com/emilnabil237/plugins/DreamSatPanel/installer.sh  -O - | /bin/sh
 
-elif [ $choice -eq 8 ]
+elif $choice -eq 8
 then
 sleep 1
 echo " download plugin Keyadder "
 wget -q "--no-check-certificate" https://dreambox4u.com/emilnabil237/KeyAdder/installer.sh -O - | /bin/sh
 
-elif [ $choice -eq 0 ]
+elif $choice -eq 0
 then
 clear
 echo " thank you "
@@ -75,6 +75,7 @@ echo ""
 echo ""
 echo "******************************************************"
 exit
+
 
 
 
