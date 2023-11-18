957866bc67a3a4703e657459e2fb01f8e5159c95
emil237 <marianatalaat1@gmail.com>
2023-11-18 20:09:02 +0200

    Add files via upload

--- multi-plugins.sh
+++ multi-plugins.sh
@@ -1,6 +1,44 @@
-#!/bin/sh
+#!/usr/bin/bash
 #
-cd /tmp
+
+# colors
+Color_Off="\e[0m" # Text Reset
+Red="\e[0;31m"    # Red
+Yellow="\e[0;33m" # Yellow
+Blue='\e[0;34m'   # Blue
+Green='\e[0;32m'  # Green
+
+###########################################
+# Configure where we can find things here #
+pyVersion=$(python -c"from sys import version_info; print(version_info[0])")
+SitUrl='https://raw.githubusercontent.com/emil237/Important-plugins/main'
+TmpDir='/var/volatile/tmp'
+
+####################
+#  Depends Checking  #
+arrVar=("ffmpeg" "gstplayer" "exteplayer3" "enigma2-plugin-systemplugins-serviceapp")
+
+if [ "${pyVersion}" = 3 ]; then
+    arrVar+=("python3-core" "python3-futures3" "python3-image" "python3-json" "python3-multiprocessing" "python3-pillow" "python3-requests" "python3-cryptography")
+else
+    arrVar+=("python-core" "python-futures" "python-image" "python-imaging" "python-json" "python-multiprocessing" "python-requests" "python-cryptography")
+fi
+for PkgFile in "${arrVar[@]}"; do
+    if ! grep -qs "Package: $PkgFile" '/var/lib/opkg/status'; then
+        echo -e ">>>>   Please Wait Install ${Green}${PkgFile}${Color_Off}   <<<<"
+        echo
+        opkg install "${PkgFile}"
+        wait
+        sleep 0.8
+        clear
+    fi
+done
+
+if [ "$(opkg info libcrypto-compat | grep -Fic Package)" = 1 ]; then
+    LibPkg="libcrypto-compat"
+else
+    LibPkg="libcrypto-compat-1.0.0"
+fi
 echo " Download Plugins "
 echo "  1 - Cccam"
     echo "  2 - Ncam"
@@ -69,20 +107,12 @@ then
 clear
 echo " thank you "
 fi
+sleep 0.8
+sync
+echo ""
+echo ""
+echo "******************************************************"
 exit
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
