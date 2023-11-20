7f87d96ec6b14135c6b1c27664ca0a8607b5b5b0
emil237 <marianatalaat1@gmail.com>
2023-11-20 21:29:56 +0200

    Add files via upload

--- multi-plugins.sh
+++ multi-plugins.sh
@@ -1,114 +1,71 @@
-#!/bin/sh
-#
-cd /tmp
-echo " Download Plugins "
-choice="$1"
-echo -e "\e[35m  1 - Cccam"
-    echo -e "\e[33m  2 - Ncam"
-    echo -e "\e[35m  3 - Oscam"
-    echo -e "\e[32m  4 - Raedquicksignal"
-    echo -e "\e[33m  5 - Arabicsavior"
-    echo -e "\e[35m  6 - Ajpanel"
-    echo -e "\e[32m  7 - Dreamsatpanel"
-    echo -e "\e[33m  8 - Keyadder"
-    echo -e "\e[31m  9 - Jedimakerxtream"
-    echo -e "\e[32m  10 - xtreamity"
-    echo -e "\e[35m  11 - Iptosat"
-    echo -e "\e[31m  12 - Ipaudio 6.7"
+#!/bin/bash
+
+while true; do
+    echo "Download Plugins"
+    echo "1 - Cccam"
+    echo "2 - Ncam"
+    echo "3 - Oscam"
+    echo "4 - Raedquicksignal"
+    echo "5 - Arabicsavior"
+    echo "6 - Ajpanel"
+    echo "7 - Dreamsatpanel"
+    echo "8 - Keyadder"
+    echo "9 - Jedimakerxtream"
+    echo "10 - xtreamity"
+    echo "11 - Iptosat"
+    echo "12 - Ipaudio 6.7"
+    echo "0 - Exit"
     echo
-    echo -e "\e[32m  0 - Exit"
-    echo
-    echo "- Enter Numper option:"
+    echo "Enter options (space-separated):"
+    read choices
 
-    read choice
-if [ "$choice" -eq 1 ]
-then
-sleep 1
-echo " download emu cccam "
-wget https://dreambox4u.com/emilnabil237/emu/installer-cccam.sh -O - | /bin/sh
+    # Split the user input into an array
+    IFS=' ' read -ra choices_array <<< "$choices"
 
-elif [ "$choice" -eq 2 ]
-then
-sleep 1
-echo " download emu ncam "
-wget https://dreambox4u.com/emilnabil237/emu/installer-ncam.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 3 ]
-then
-sleep 1
-echo " download emu oscam "
-wget https://dreambox4u.com/emilnabil237/emu/installer-oscam.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 4 ]
-then
-sleep 1
-echo " download plugin Raedquicksignal "
-wget https://dreambox4u.com/emilnabil237/plugins/RaedQuickSignal/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 5 ]
-then
-sleep 1
-echo " download plugin Arabicsavior "
-wget http://dreambox4u.com/emilnabil237/plugins/ArabicSavior/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 6 ]
-then
-sleep 1
-echo " download plugin Ajpanel "
-wget -q "--no-check-certificate" http://dreambox4u.com/emilnabil237/plugins/ajpanel/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 7 ]
-then
-sleep 1
-echo " download plugin Dreamsat Panel "
-wget -q "--no-check-certificate" http://dreambox4u.com/emilnabil237/plugins/DreamSatPanel/installer.sh  -O - | /bin/sh
-
-elif [ "$choice" -eq 8 ]
-then
-sleep 1
-echo " download plugin Keyadder "
-wget -q "--no-check-certificate" https://dreambox4u.com/emilnabil237/KeyAdder/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 9 ]
-then
-sleep 1
-echo " download plugin Jedimakerxtream "
-wget https://dreambox4u.com/emilnabil237/plugins/jedimakerxtream/installer.sh -qO - | /bin/sh
-
-elif [ "$choice" -eq 10 ]
-then
-sleep 1
-echo " download plugin xtreamity "
-wget https://dreambox4u.com/emilnabil237/plugins/xtreamity/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 11 ]
-then
-sleep 1
-echo " download plugin Iptosat "
-wget -q "--no-check-certificate" https://dreambox4u.com/emilnabil237/plugins/iptosat/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 12 ]
-then
-sleep 1
-echo " download plugin Ipaudio 6.7"
-wget -q "--no-check-certificate" https://dreambox4u.com/emilnabil237/plugins/ipaudio/installer.sh -O - | /bin/sh
-
-elif [ "$choice" -eq 0 ]
-then
-clear
-echo -e "\e[32m thank you  And Good By "
- fi
-echo -e "\e[35mUploded By Emil Nabil "
-sleep 3;
-exit
-
-
-
-
-
-
-
-
-
-
+    # Process each choice
+    for choice in "${choices_array[@]}"; do
+        case $choice in
+            1)
+                echo "Running emu cccam script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/emu/installer-cccam.sh)" ;;
+            2)
+                echo "Running emu ncam script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/emu/installer-ncam.sh)" ;;
+            3)
+                echo "Running emu oscam script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/emu/installer-oscam.sh)" ;;
+            4)
+                echo "Running plugin Raedquicksignal script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/plugins/RaedQuickSignal/installer.sh)" ;;
+            5)
+                echo "Running plugin Arabicsavior script from URL..."
+                sh -c "$(wget -O- http://dreambox4u.com/emilnabil237/plugins/ArabicSavior/installer.sh)" ;;
+            6)
+                echo "Running plugin Ajpanel script from URL..."
+                sh -c "$(wget -qO- --no-check-certificate http://dreambox4u.com/emilnabil237/plugins/ajpanel/installer.sh)" ;;
+            7)
+                echo "Running plugin Dreamsat Panel script from URL..."
+                sh -c "$(wget -qO- --no-check-certificate http://dreambox4u.com/emilnabil237/plugins/DreamSatPanel/installer.sh)" ;;
+            8)
+                echo "Running plugin Keyadder script from URL..."
+                sh -c "$(wget -qO- --no-check-certificate https://dreambox4u.com/emilnabil237/KeyAdder/installer.sh)" ;;
+            9)
+                echo "Running plugin Jedimakerxtream script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/plugins/jedimakerxtream/installer.sh)" ;;
+            10)
+                echo "Running plugin xtreamity script from URL..."
+                sh -c "$(wget -O- https://dreambox4u.com/emilnabil237/plugins/xtreamity/installer.sh)" ;;
+            11)
+                echo "Running plugin Iptosat script from URL..."
+                sh -c "$(wget -qO- --no-check-certificate https://dreambox4u.com/emilnabil237/plugins/iptosat/installer.sh)" ;;
+            12)
+                echo "Running plugin Ipaudio 6.7 script from URL..."
+                sh -c "$(wget -qO- --no-check-certificate https://dreambox4u.com/emilnabil237/plugins/ipaudio/installer.sh)" ;;
+            0)
+                echo "Thank you"; exit ;;
+            *)
+                echo "Invalid option: $choice"; continue ;;
+        esac
+    done
+done
 
